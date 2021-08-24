6867c694bbfdb52d817bd10ac48135a6a2c5860c
Pavel <wol1414@gmail.com>
2021-08-24 20:47:58 +0300

    update libs

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -53,7 +53,7 @@ const JSCCommon = {
 		// });
 
 		$(".modal-close-js").click(function () {
-			fancybox.close();
+			Fancybox.close();
 		})
 		// fancybox.defaults.backFocus = false;
 		const linkModal = document.querySelectorAll(link);
@@ -213,7 +213,7 @@ const JSCCommon = {
 				data: data,
 			}).done(function (data) {
 
-				fancybox.close();
+				Fancybox.close();
 				Fancybox.show([{ src: "#modal-thanks", type: "inline" }]);
 				// window.location.replace("/thanks.html");
 				setTimeout(function () {
